e7dc72c252524d0451be0329db6ef641809a1c86
iborhim008 <sunnat5809@gmail.com>
2026-05-14 00:15:14 +0500

    cors added

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express()
+app.use(cors('http://localhost:5173/'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
